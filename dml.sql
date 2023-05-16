882934386d0ee88256d92f5e841f17d7dceda348
Brian Vo <Vobrian025@gmail.com>
2023-05-16 19:10:02 -0400

    commas

--- dml.sql
+++ dml.sql
@@ -9,38 +9,38 @@ INSERT INTO customer(
 );
 
 INSERT INTO Movies(
-    movie_id
-    title
-    run_time
+    movie_id,
+    title,
+    run_time,
     rating
 )VALUES(
-    '1'
+    '1',
     'The super mario bros',
-    '1 Hour 32 Minutes'
+    '1 Hour 32 Minutes',
     'PG'
 );
 
 INSERT INTO Tickets(
-    price
-    quantity
-    customer_id
+    price,
+    quantity,
+    customer_id,
     movie_id
 )VALUES(
-    8
-    1
-    1
+    8,
+    1,
+    1,
     1
 );
 
 INSERT INTO Concession(
-    Concession_id
-    price
-    Product
+    Concession_id,
+    price,
+    Product,
     customer_id
 )VALUES(
-    1
-    10
-    'Popcorn'
+    1,
+    10,
+    'Popcorn',
     1
 );
 
